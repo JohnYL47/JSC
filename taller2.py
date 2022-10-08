adee11ddacd8b2afe7dce4b31d73b29143d3e61c
JohnYL47 <jylaguna6@misena.edu.co>
2022-10-08 14:03:33 -0500

    Taller Py

---
+++ taller2.py
@@ -0,0 +1,3 @@
+alumn = input("Ingrese el nombre y aplleido del estudiante: ")
+a = int(input("Ingrese las notas en total de los 4tos periodos: "))
+print("el estudiante " + alumn + "tiene un total de los 4tos periodos: " + str(a))
