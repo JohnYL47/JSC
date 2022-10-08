6165b689fdde8767aa1e0cebe403b050f5920271
JohnYL47 <106090431+JohnYL47@users.noreply.github.com>
2022-10-08 14:15:46 -0500

    Update taller2.py

--- taller2.py
+++ taller2.py
@@ -1,3 +1,5 @@
 alumn = input("Ingrese el nombre y aplleido del estudiante: ")
 a = int(input("Ingrese las notas en total de los 4tos periodos: "))
 print("el estudiante " + alumn + "tiene un total de los 4tos periodos: " + str(a))
+
+print("Hellou your are can fiuter")
